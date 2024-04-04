743b00ee424092d58f5b7dcb32d3eb6ee1d2202f
Ifeanyi Zion <ifeanyizion3@gmail.com>
2024-04-04 12:29:42 -0700

    using fabric to archive a file

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,19 +2,22 @@
 """
     Module packs a file into an archive
 """
+
+
 import os
 import datetime
 from fabric.api import *
+
 
 def do_pack():
     """function to archive a file recursively"""
     current = datetime.datetime.now()
     filename = "web_static_{}{}{}{}{}{}.tgz".format(current.year,
-                                      current.month,
-                                      current.day,
-                                      current.hour,
-                                      current.minute,
-                                      current.second)
+                                                    current.month,
+                                                    current.day,
+                                                    current.hour,
+                                                    current.minute,
+                                                    current.second)
     local("mkdir -p /versions/")
     try:
         print("Packing web_static to {}".format(filename))
