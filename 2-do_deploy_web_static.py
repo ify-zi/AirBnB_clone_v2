2fc2b15776b6c795f6f6ff5cd131f98a8d86cfe3
Ifeanyi Zion <ifeanyizion3@gmail.com>
2024-04-04 14:14:07 -0700

    deploy web static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -21,14 +21,15 @@ def do_deploy(archive_path):
     filename = os.path.basename(archive_path)
     foldername = filename.replace(".tgz", "")
     folderpath = "/data/web_static/releases/{}/".format(foldername)
-    status = True
     try:
         put("archive_path /tmp/{}".format(filename))
         run("sudo mkdir -p /data/web_static/releases/{}".format(foldername))
         run("sudo tar -xvf /tmp/{} -C {}".format(filename, folderpath))
         run("sudo rm -rf /tmp/{}".format(filename))
+        run("mv {}web_static/* {}".format(folder_path, folder_path))
+        run("rm -rf {}web_static".format(folder_path))
         run("sudo rm -rf /data/web_static/current")
         run("sudo ln -s {} /data/web_static/current".format(folderpath))
+        return True
     except Exception:
-        status = False
-    return status
+        return False
