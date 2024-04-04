ea282d6ec1930827f8227821442eca5b191892d7
Ifeanyi Zion <ifeanyizion3@gmail.com>
2024-04-04 14:44:41 -0700

    deploy static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,7 +8,7 @@ import os
 from fabric.api import *
 
 
-env.host = ['52.87.219.34', '100.25.132.43']
+env.hosts = ['52.87.219.34', '100.25.132.43']
 
 
 def do_deploy(archive_path):
@@ -22,14 +22,15 @@ def do_deploy(archive_path):
     foldername = filename.replace(".tgz", "")
     folderpath = "/data/web_static/releases/{}/".format(foldername)
     try:
-        put("archive_path /tmp/{}".format(filename))
-        run("sudo mkdir -p /data/web_static/releases/{}".format(foldername))
-        run("sudo tar -xvf /tmp/{} -C {}".format(filename, folderpath))
+        put(archive_path, "/tmp/{}".format(filename))
+        run("sudo mkdir -p {}".format(folderpath))
+        run("sudo tar -xzf /tmp/{} -C {}".format(filename, folderpath))
         run("sudo rm -rf /tmp/{}".format(filename))
-        run("mv {}web_static/* {}".format(folderpath, folderpath))
-        run("rm -rf {}web_static".format(folderpath))
+        run("sudo mv {}web_static/* {}".format(folderpath, folderpath))
+        run("sudo rm -rf {}web_static".format(folderpath))
         run("sudo rm -rf /data/web_static/current")
         run("sudo ln -s {} /data/web_static/current".format(folderpath))
+        print("Deployed")
         return True
     except Exception:
         return False
