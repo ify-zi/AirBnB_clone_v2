8142c86686853d153982feb0d96eaa6ace41e9fc
Ifeanyi Zion <ifeanyizion3@gmail.com>
2024-04-04 12:51:50 -0700

    using fabric to archive files

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,22 +6,25 @@
 
 import os
 from datetime import datetime
-from fabric.api import *
+from fabric.api import local, runs_once
 
 
+@runs_once
 def do_pack():
     """function to archive a file recursively"""
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
     current = datetime.now()
-    filename = "web_static_{}{}{}{}{}{}.tgz".format(current.year,
-                                                    current.month,
-                                                    current.day,
-                                                    current.hour,
-                                                    current.minute,
-                                                    current.second)
+    filename = "versions/web_static_{}{}{}{}{}{}.tgz".format(current.year,
+                                                             current.month,
+                                                             current.day,
+                                                             current.hour,
+                                                             current.minute,
+                                                             current.second)
     try:
-        local("mkdir -p /versions")
         print("Packing web_static to {}".format(filename))
-        local("tar -cvzf /versions/{} ./web_static".format(filename))
-        return "/versions/{}".format(filename)
+        local("tar -cvzf {} web_static".format(filename))
+        file_size = os.stat(filename).st_size
+        print("web_static packed: {} -> {} Bytes".format(filename, file_size))
     except Exception:
         return "None"
