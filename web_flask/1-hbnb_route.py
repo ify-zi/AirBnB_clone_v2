a616d93fcd21f924a512427a62764bf6c09e9bc3
Ifeanyi Zion <ifeanyizion3@gmail.com>
2024-04-18 14:41:59 -0700

    pep 8 correction

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -16,5 +16,13 @@ def welcome():
     return "Hello HBNB"
 
 
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+        prints the hbnb page
+    """
+    return "HBNB"
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
