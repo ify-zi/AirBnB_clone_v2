e884b479bac7f2557c9855787019dabf6b137e3f
Ifeanyi Zion <ifeanyizion3@gmail.com>
2024-04-18 14:21:40 -0700

    launching flask, resolved installation errors

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    Web Flask Module - Script start a flask app
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    """
+        prints the home page
+    """
+    return "Hello HBNB"
+
+if __name__== '__main__':
+    app.run(debug=True, host='0.0.0.0')
