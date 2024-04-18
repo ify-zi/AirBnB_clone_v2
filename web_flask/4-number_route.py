2469c5312eba677b33ed50fa146487cae5d86b98
Ifeanyi Zion <ifeanyizion3@gmail.com>
2024-04-18 15:55:33 -0700

    number routing page

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+    Web Flask Module - Script start a flask app
+"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+""" Instantiation of the code """
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def welcome():
+    """
+        prints the home page
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """
+        prints the hbnb page
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    """
+        Using Variables for routing
+    """
+    return "C {:s}".format(text.replace('_', ' '))
+
+
+@app.route('/python/<text>')
+@app.route('/python', defaults={"text": "is cool"})
+def python_func(text):
+    """
+        printing varaible swith conditions
+    """
+    return "Python {:s}".format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>')
+def number_func(n):
+    """check for number and return page"""
+    return "{} is a number".format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
