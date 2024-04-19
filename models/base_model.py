25bca58d2af7018310c30ec109132084d8af9f49
Ifeanyi Zion <ifeanyizion3@gmail.com>
2024-04-19 01:03:27 -0700

    checking again

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,8 @@ from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DATETIME
 
+Base = declarative_base()
+
 class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), nullable=False, primary_key=True, unique=True)
