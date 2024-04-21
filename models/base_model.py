6d96043f7642b91f1f7b6f916a519cfc2d1c9f06
Ifeanyi Zion <ifeanyizion3@gmail.com>
2024-04-21 08:00:46 -0700

    correction

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 
+import os
 import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
